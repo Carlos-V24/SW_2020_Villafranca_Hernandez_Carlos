8b844cbef415097dfeb8a47315fdf3024023b591
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2020-05-14 13:02:28 -0500

    FEAT: Se agrego al repositorio Actividad2.php

---
+++ Actividad2.php
@@ -0,0 +1,6 @@
+
+<?php
+echo "Lo que mas me ha gustado del curso web es:<br>";
+echo "1.- Aprender Karel, es bastante divertido.<<br>";
+echo "2.- Aprender cookies y seiones porque el reto ya lo habia medio visto";
+ ?>
